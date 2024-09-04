90bfa16a04629e98844a74dce9cb9843785e45a2
Andrei Lazakovitch <alazakovitch@gmail.com>
2024-09-03 20:29:48 -0400

    Updated documentation

--- lib.go
+++ lib.go
@@ -1,5 +1,8 @@
+// A sample package to test imports.
 package go_test
 
+// Add two numbers together.
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
   return a + b
 }

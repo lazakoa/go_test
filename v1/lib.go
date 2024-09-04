37ad0146481b5258be47d674527a9f743847e9f2
Andrei Lazakovitch <alazakovitch@gmail.com>
2024-09-03 20:53:23 -0400

    Version 2 updated.

---
+++ v1/lib.go
@@ -0,0 +1,12 @@
+// A sample package to test imports.
+package go_test
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+// Add two numbers together.
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
+func Add(a int, b int) {
+  return a + b
+}

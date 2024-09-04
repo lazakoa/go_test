45973e3739f6dc70118629278519df92e46152c8
Andrei Lazakovitch <alazakovitch@gmail.com>
2024-09-03 20:45:59 -0400

    Added a generic version of Add

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lazakoa/go_test
 
 go 1.23.0
+
+require golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
--- lib.go
+++ lib.go
@@ -1,8 +1,16 @@
 // A sample package to test imports.
 package go_test
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add two numbers together.
 // [Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a int, b int) int {
-  return a + b
+func Add[T Number](a T, b T) T {
+	return a + b
 }
